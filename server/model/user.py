99a8a97108e60e1a5cebbc54b53707607eddecb8
Chander Ramesh <hardly.hacking@gmail.com>
2018-05-05 00:38:18 -0700

    Remove unnecessary server folder

--- server/model/user.py
+++
@@ -1,8 +0,0 @@
-class User(object):
-    def __init__(self, name, pwd, games=None):
-        self.name = name
-        self.pwd = pwd
-        self.games = if games is None: [] else games
-
-    def add_game(self, game):
-        self.games.append(game)
